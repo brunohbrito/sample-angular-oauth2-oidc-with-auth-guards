ccf330cf6d6b5a12e16c83bdfd3f3bcdab3cbefd
Bruno Brito <bhdebrito@gmail.com>
2019-12-17 13:21:39 -0300

    Update auth-config.ts

--- src/app/core/auth-config.ts
+++ src/app/core/auth-config.ts
@@ -1,12 +1,12 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
-  issuer: 'http://sso.teste.work',
+  issuer: 'https://sso.teste.work',
   clientId: 'angular-demo',
   redirectUri: window.location.origin + '/index.html',
   silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
   scope: 'openid profile email api_demo.api jp_api.user',
-  requireHttps: false,
+  requireHttps: true,
   silentRefreshTimeout: 5000, // For faster testing
   timeoutFactor: 0.25, // For faster testing
   sessionChecksEnabled: true,
